54b9543706dc13da4434064f160c03686957f086
mahavishnu <mahanvishnu3@gamil.com>
2025-12-04 18:15:12 +0530

    update

--- src/footer.jsx
+++ src/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const footer = () => {
   return (
     <div className='footer'>
-      designed By <a className='footer-name' href="">Mahan Prince Oficial</a>
+      designed By <a className='footer-name' href="https://mahanfreelauncer.vercel.app/">Mahan Prince Oficial</a>
     </div>
   )
 }
